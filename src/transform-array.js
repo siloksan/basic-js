deb447e333ad70de88700ec8cb35e3bca2064f59
siloksan <tutel07@gmail.com>
2023-12-07 12:51:56 +0300

    feat: implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,58 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function transform(arr) {
+  if (!(arr instanceof Array)) {
+    throw Error("'arr' parameter must be an instance of the Array!");
+  }
+  let control = true;
+  const transformArray = [];
+  arr.forEach((item, index) => {
+    if (item === "--discard-next") {
+      control = false;
+    } else if (item === "--discard-prev") {
+      if (transformArray[transformArray.length - 1] === arr[index - 1]) {
+        transformArray.pop();
+      }
+    } else if (item === "--double-next") {
+      if (
+        arr[index + 1] &&
+        arr[index - 1] === transformArray[transformArray.length - 1]
+      ) {
+        transformArray.push(arr[index + 1]);
+      } else {
+        return;
+      }
+    } else if (item === "--double-prev") {
+      if (
+        arr[index - 1] &&
+        arr[index - 1] === transformArray[transformArray.length - 1]
+      ) {
+        transformArray.push(arr[index - 1]);
+      } else {
+        return;
+      }
+    } else {
+      control && transformArray.push(item);
+      control = true;
+    }
+  });
+  return transformArray;
 }
 
 module.exports = {
-  transform
+  transform,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -33,61 +33,6 @@ function getSeason(date) {
   }
 }
 
-// function getSeason(date) {
-//   if (!date) return "Unable to determine the time of year!";
-//   if (!(date instanceof Date) || isNaN(date)) throw new Error("Invalid date!");
-
-//   const month = date.getMonth();
-//   if (month === 11 || month <= 1) return "winter";
-//   if (month <= 4) return "spring";
-//   if (month <= 7) return "summer";
-//   if (month <= 10) return "autumn";
-
-//   return "Invalid date!";
-// }
-
 module.exports = {
   getSeason,
 };
-// const fakeDate = {
-//   toString() {
-//     return Date.prototype.toString.call(new Date());
-//   },
-//   [Symbol.toStringTag]: "Date",
-// };
-// let time = getSeason(new Date(1994, 8, 26, 3, 0, 11, 500));
-// console.log(time);
-
-// time = getSeason(new Date(2150, 7, 21, 18, 36, 41, 841));
-// console.log(time);
-
-// (time = getSeason("foo")), console.log("time: ", time);
-// (time = getSeason({ John: "Smith" })), console.log("time: ", time);
-
-// (time = getSeason(20192701)), console.log("time: ", time);
-// const time = new Date();
-// console.log("time.toString(): ", Object.toString(new Date()));
-
-// Object.getOwnPropertyNames(new Date());
-// console.log(
-//   "Object.getOwnPropertyNames(): ",
-//   Object.getOwnPropertyNames(new Date())
-// );
-// (time = getSeason(() => new Date())), console.log("time: ", time);
-
-// time = new Date();
-// console.log("time: ", time);
-// console.log(
-//   "Object.prototype.toString.call(new Date()): ",
-//   Object.prototype.toString.call(time)
-// );
-
-// const fakeDate = {
-//   toString() {
-//     return Date.prototype.toString.call(new Date());
-//   },
-//   [Symbol.toStringTag]: "Date",
-// };
-
-// Object.setPrototypeOf(fakeDate, Object.getPrototypeOf(new Date()));
-// console.log('getSeason(fakeDate): ', getSeason(fakeDate));
